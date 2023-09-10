88c106fa3ac829d4bcc4265f58c6b0d105b954cc
Apgarbs <alvinpaul136@gmail.com>
2023-09-10 14:26:55 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Point of code entry
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	int s;
+
+	s = '9';
+	while
+		(s <= '9') {
+	putchar(s);
+	if
+	(s <= '8') {
+	putchar(',');
+	putchar(' ');
+	}
+	s++;
+	}
+	putchar('\n');
+	return (0);
+}
