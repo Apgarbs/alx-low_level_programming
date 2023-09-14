5f07158784df579fa35dad52e08e71f65749deae
Apgarbs <alvinpaul136@gmail.com>
2023-09-13 18:06:15 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description:  finds and prints the sum of the even-valued terms
+*
+* Return: 0 (success)
+*
+*/
+int main(void)
+{
+ong int n, a = 1, b = 2, sums = 0, totsum = 0;
+
+for (n = 0; n < 49; n++)
+
+if ((b % 2 == 0) && (b <= 4000000))
+{
+totsum = totsum + b;
+}
+sums = a + b;
+a = b;
+b = sums;
+}
+printf("%ld\n", totsum);
+return (0);
+}
+
+
