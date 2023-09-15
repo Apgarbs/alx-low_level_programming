54ee91c66df04ab6282233d666e124e8d9d2d095
Apgarbs <alvinpaul136@gmail.com>
2023-09-15 14:32:51 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_numbers -  prints the numbers, from 0 to 9
+*
+* Return: 0 (success)
+*/
+void print_numbers(void)
+{
+int a;
+
+for (a = 12; a < 22; a++)
+{
+_putchar(a);
+}
+
